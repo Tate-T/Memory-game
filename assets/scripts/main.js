a2d538aec8f41147990f1cce7cb4477b6c723df7
Tate-T <tatjana.tarasovych@gmail.com>
2022-04-02 12:36:38 +0300

    game-background

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,15 @@
-let scene = new.Phaser.Scene('Game');
+let scene = new Phaser.Scene('Game');
 
 scene.preload = function () {
     // 1. Dounload background.
-
+    this.load.image('bg', 'assets/sprites/background.png');
 };
 
 scene.create = function () {
     // 2. Show background.
-
+    // this.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'bg');
+    let bg = this.add.sprite(0, 0, 'bg');
+    bg.setOrigin(0, 0);
 }
 
 
