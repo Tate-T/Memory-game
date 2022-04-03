e038e1bb03c69a27a5cad2f2d49acae2038e4312
Tate-T <tatjana.tarasovych@gmail.com>
2022-04-03 11:24:50 +0300

    added class GameScene

---
+++ assets/scripts/GameScene.js
@@ -0,0 +1,88 @@
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super('Game');
+    }
+
+    preload() {
+        // 1. Dounload background.
+        this.load.image('bg', 'assets/sprites/background.png');
+        this.load.image('card', 'assets/sprites/card.png');
+    }
+
+    create() {
+        // 2. Show background.
+        // this.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'bg');
+        this.add.sprite(0, 0, 'bg').setOrigin(0, 0);
+
+        let positions = this.getCardsPosition();
+        for (let position of positions) {
+            this.add.sprite(position.x, position.y, 'card').setOrigin(0, 0);
+        }
+    }
+
+    getCardsPosition() {
+        let cardsPosition = [];
+        // let cardTexture = this.textures.get('card').getSourceImage();
+        // let cardWidth = cardTexture.width + 4;
+        // let cardHeight = cardTexture.height + 4;
+        let cardWidth = 280 + 4;
+        let cardHeight = 280 + 4;
+        // let offsetX = (this.sys.game.config.width - cardWidth * config.cols) / 2;
+        // let offsetY = (this.sys.game.config.heigth - cardHeight * config.rows) / 2;
+        let offsetX = 55;
+        let offsetY = 20;
+
+
+        for (let row = 0; row < config.rows; row++) {
+            for (let col = 0; col < config.cols; col++) {
+                cardsPosition.push({
+                    x: offsetX + col * cardWidth,
+                    y: offsetY + row * cardHeight,
+                });
+            }
+        }
+
+        return cardsPosition;
+    }
+}
+
+    //     scene.preload = function () {
+    //         this.load.image('bg', 'assets/sprites/background.png');
+    //         this.load.image('card', 'assets/sprites/card.png');
+    //    };
+
+    // scene.create = function () {
+    //     // 2. Show background.
+    //     // this.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'bg');
+    //     this.add.sprite(0, 0, 'bg').setOrigin(0, 0);
+
+    //     let positions = this.getCardsPosition();
+    //     for (let position of positions) {
+    //         this.add.sprite(position.x, position.y, 'card').setOrigin(0, 0);
+    //     }
+    // };
+
+    // scene.getCardsPosition = function () {
+    //     let cardsPosition = [];
+    //     // let cardTexture = this.textures.get('card').getSourceImage();
+    //     // let cardWidth = cardTexture.width + 4;
+    //     // let cardHeight = cardTexture.height + 4;
+    //     let cardWidth = 280 + 4;
+    //     let cardHeight = 280 + 4;
+    //     // let offsetX = (this.sys.game.config.width - cardWidth * config.cols) / 2;
+    //     // let offsetY = (this.sys.game.config.heigth - cardHeight * config.rows) / 2;
+    //     let offsetX = 55;
+    //     let offsetY = 20;
+
+
+    //     for (let row = 0; row < config.rows; row++) {
+    //         for (let col = 0; col < config.cols; col++) {
+    //             cardsPosition.push({
+    //                 x: offsetX + col * cardWidth,
+    //                 y: offsetY + row * cardHeight,
+    //             });
+    //         }
+    //     }
+
+    //     return cardsPosition;
+    // };
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,53 +1,22 @@
-let scene = new Phaser.Scene('Game');
-
-scene.preload = function () {
-    // 1. Dounload background.
-    this.load.image('bg', 'assets/sprites/background.png');
-    this.load.image('card', 'assets/sprites/card.png');
-};
-
-scene.create = function () {
-    // 2. Show background.
-    // this.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'bg');
-    this.add.sprite(0, 0, 'bg').setOrigin(0, 0);
-
-    let positions = this.getCardsPosition();
-    for (let position of positions) {
-        this.add.sprite(position.x, position.y, 'card').setOrigin(0, 0);
-    }
-};
-
-scene.getCardsPosition = function () {
-    let cardsPosition = [];
-    // let cardTexture = this.textures.get('card').getSourceImage();
-    // let cardWidth = cardTexture.width + 4;
-    // let cardHeight = cardTexture.height + 4;
-    let cardWidth = 280 + 4;
-    let cardHeight = 280 + 4;
-    // let offsetX = (this.sys.game.config.width - cardWidth * config.cols) / 2;
-    // let offsetY = (this.sys.game.config.heigth - cardHeight * config.rows) / 2;
-    let offsetX = 55;
-    let offsetY = 20;
-
-
-    for (let row = 0; row < config.rows; row++) {
-        for (let col = 0; col < config.cols; col++) {
-            cardsPosition.push({
-                x: offsetX + col * cardWidth,
-                y: offsetY + row * cardHeight,
-            });
-        }
-    }
-
-    return cardsPosition;
-};
-
 let config = {
     type: Phaser.AUTO, // webgl or canvas
     width: 1200,
     height: 720,
     rows: 2,
     cols: 4,
-    scene: scene,
+    scene: new GameScene(),
 }
 let game = new Phaser.Game(config);
+
+// let scene = new GameScene();
+
+// let config = {
+//     type: Phaser.AUTO, // webgl or canvas
+//     width: 1200,
+//     height: 720,
+//     rows: 2,
+//     cols: 4,
+//     scene: scene,
+// }
+// let game = new Phaser.Game(config);
+
